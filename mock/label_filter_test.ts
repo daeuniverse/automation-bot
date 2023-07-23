70a762a10d0290d093f8bbaab79556211081f42d
kev <yqlbu@bu.edu>
2023-07-23 23:29:52 +0800

    fixture(mock): rework label_filter_test

--- mock/label_filter_test.ts
+++ mock/label_filter_test.ts
@@ -13,18 +13,27 @@ const defaultLables = [
   "fixture",
 ];
 
+const overwriteLabels = (input: string[]): string[] => {
+  return input.map((item) => {
+    if (item == "feat") item = "feature";
+    if (item == "docs" || item == "doc") item = "documentation";
+    return item;
+  });
+};
+
 const testLabelRegex = (input: string): string[] => {
-  return defaultLables
-    .filter((label: string) => {
-      const re = /^(?<type>\w+)(\((?<scope>.+)\))?:/;
-      const { type } = input.match(re)?.groups!;
-      return type === label;
-    })
-    .map((item) => {
-      if (item == "feat") item = "feature";
-      if (item == "docs" || item == "doc") item = "documentation";
-      return item;
-    });
+  // const re = /^(?<type>\w+)(\/\w+)*(\((?<scope>.+)\))?:/;
+  const re = /^(?<type>\w+(\/\w+)*)(\((?<scope>.+)\))?:/;
+  const { type } = input.match(re)?.groups!;
+  const labels = type.split("/");
+
+  return labels.length > 1
+    ? overwriteLabels(
+        labels.map(
+          (label: string) => defaultLables.filter((x: string) => label === x)[0]
+        )
+      )
+    : overwriteLabels(defaultLables.filter((x: string) => type === x));
 };
 
 const prTitle1 = "fixture(context): some title";
@@ -32,9 +41,11 @@ const prTitle2 = "fix(context): some title";
 const prTitle3 = "docs(context): some title";
 const prTitle4 = "docu(context): some title";
 const prTitle5 = "feat: display daed version in header";
+const prTitle6 = "feat/fix/ci(context): some title;";
 
 console.log(testLabelRegex(prTitle1));
 console.log(testLabelRegex(prTitle2));
 console.log(testLabelRegex(prTitle3));
 console.log(testLabelRegex(prTitle4));
 console.log(testLabelRegex(prTitle5));
+console.log(testLabelRegex(prTitle6));
